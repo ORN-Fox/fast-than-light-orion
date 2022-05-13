cfa934cac8706878e9e0a97792018a929f4ff7c1
Florian Armand <foxdog.orion@gmail.com>
2022-05-13 13:32:22 -0400

    feat: test display of ship in shed canvas

--- src/app/views/shed/shed.component.ts
+++ src/app/views/shed/shed.component.ts
@@ -10,6 +10,18 @@ export class ShedComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const shedCanvas = document.querySelector('#shedCanvas') as HTMLCanvasElement;
+    shedCanvas.height = 768;
+    shedCanvas.width = 1366;
+
+    const ctx = shedCanvas.getContext('2d') as CanvasRenderingContext2D;
+
+    // TEMP
+    let shipImage = new Image();
+    shipImage.onload = function() {
+      ctx.drawImage(shipImage, 400, 100);
+    };
+    shipImage.src = '/assets/images/ships/kestrel_cruiser_a.webp';
   }
 
 }
