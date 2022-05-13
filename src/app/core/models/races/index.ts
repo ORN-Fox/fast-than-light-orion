d7e047f7dfeeade349dceb79391c063fa0035951
Florian Armand <foxdog.orion@gmail.com>
2022-05-13 17:16:49 -0400

    feat: add global export for several races models

---
+++ src/app/core/models/races/index.ts
@@ -0,0 +1,10 @@
+export * from './race.model';
+
+export * from './crystal/crystal.model';
+export * from './engi/engi.model';
+export * from './human/human.model';
+export * from './lanius/lanius.model';
+export * from './mantis/mantis.model';
+export * from './rockmen/rockmen.model';
+export * from './slug/slug.model';
+export * from './zoltan/zoltan.model';
