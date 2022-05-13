e7b57c52e7477df913936b4d72129d1dbda030ce
Florian Armand <foxdog.orion@gmail.com>
2022-05-13 17:06:03 -0400

    feat: manage crew and system for room constructor model

--- src/app/core/models/room/room.model.ts
+++ src/app/core/models/room/room.model.ts
@@ -1,10 +1,13 @@
 import { Crew } from '../crew/crew.model';
+import { System } from '../systems/system.model';
 
 export class Room {
 
-  affectedCrew: Crew;
+  affectedCrew: Crew | null;
+  affectedSystem: System;
 
-  constructor() {
-
+  constructor(system: System, crew: Crew | null = null) {
+    this.affectedCrew = crew;
+    this.affectedSystem = system;
   }
 }
