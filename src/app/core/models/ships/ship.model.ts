148274bd7c4290e8cd0f3da615aa18dfb9a806d4
Florian Armand <foxdog.orion@gmail.com>
2022-05-13 13:39:39 -0400

    feat: refactor ship model for rework models relation and properties

---
+++ src/app/core/models/ships/ship.model.ts
@@ -0,0 +1,36 @@
+import { Room } from '../room/room.model';
+import { Weapon } from '../weapon/weapon.model';
+import { Drone } from '../drone/drone.model';
+import { Crew } from '../crew/crew.model';
+import { Upgrade } from '../upgrade/upgrade.model';
+
+export class Ship {
+
+  name: string;
+  layout: string;
+
+  hull: number;
+  reactorPower: number;
+  fuel: number;
+  missiles: number;
+  droneParts: number;
+
+  crews: Crew[] = [];
+  rooms: Room[] = [];
+  weapons: Weapon[] = [];
+  drones: Drone[] = [];
+  upgrades: Upgrade[] = [];
+
+  // Capacities
+  maxHull: number;
+  maxSystemsAllowed: number;
+  maxWeaponsAllowed: number;
+  maxDronesAllowed: number;
+  maxCrewsAllowed: number;
+  maxUpgradesAllowed: number;
+
+  constructor() {
+
+  }
+
+}
