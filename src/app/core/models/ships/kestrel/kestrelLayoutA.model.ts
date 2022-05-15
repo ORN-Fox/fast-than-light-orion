1181d5cbafea02e8a9b88056c3248a9b22567475
Florian Armand <foxdog.orion@gmail.com>
2022-05-14 21:03:42 -0400

    feat: reorder system and system level for kestrel layout A

--- src/app/core/models/ships/kestrel/kestrelLayoutA.model.ts
+++ src/app/core/models/ships/kestrel/kestrelLayoutA.model.ts
@@ -28,14 +28,14 @@ export class KestrelLayoutA extends Ship {
     ];
 
     this.rooms = [
-      new Room(new Piloting(), this.crews[0]),
-      new Room(new Door()),
-      new Room(new Sensor()),
-      new Room(new Medbay()),
+      new Room(new Shield(2)),
+      new Room(new Engine(2), this.crews[1]),
       new Room(new Oxygen()),
-      new Room(new Shield()),
-      new Room(new Engine(), this.crews[1]),
-      new Room(new WeaponControl(), this.crews[2])
+      new Room(new WeaponControl(3), this.crews[2]),
+      new Room(new Medbay()),
+      new Room(new Piloting(), this.crews[0]),
+      new Room(new Sensor()),
+      new Room(new Door())
     ];
 
     this.weapons = [
