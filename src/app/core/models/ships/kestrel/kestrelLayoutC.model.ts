944590f18c327521ba8f93e234d9065c6a260003
Florian Armand <foxdog.orion@gmail.com>
2022-05-24 10:08:20 -0400

    feat: add ship representation for kestrel layout C

--- src/app/core/models/ships/kestrel/kestrelLayoutC.model.ts
+++ src/app/core/models/ships/kestrel/kestrelLayoutC.model.ts
@@ -5,6 +5,7 @@ import { Human, Lanius } from '../../races/index';
 
 import { Room, RoomDisplaySettings } from '../../room/index';
 import { Battery, Cloacking, CloneBay, DroneControl, DoorControl, Engine, Hacking, MindControl, Oxygen, Piloting, Sensor, Shield, SystemPositionEnum, Teleport, WeaponControl } from '../../systems/index';
+import { Slot } from '../../slot/slot.model';
 
 import { DualLasers, IonStunner } from '../../weapons/index';
 
@@ -27,25 +28,44 @@ export class KestrelLayoutC extends KestrelShip {
       new Crew('Lanius', new Lanius(), Gender.Other)
     ];
 
+    let shieldRoom = new Room(new RoomDisplaySettings(198, 211, 2, 2), new Shield(2, SystemPositionEnum.Left, 3)),
+      enginesRoom = new Room(new RoomDisplaySettings(59, 176, 2, 2), new Engine(2, SystemPositionEnum.Bottom), this.crews[1]), // Human Female
+      oxygenRoom = new Room(new RoomDisplaySettings(302, 176, 1, 2), new Oxygen(1, 6)),
+      weaponsControlRoom = new Room(new RoomDisplaySettings(198, 141, 2, 2), new WeaponControl(2, SystemPositionEnum.Top, 2), this.crews[2]), // Lanius
+      pilotingRoom = new Room(new RoomDisplaySettings(547, 176, 1, 2), new Piloting(1, SystemPositionEnum.Right), this.crews[0]), // Human Male
+      sensorRoom = new Room(new RoomDisplaySettings(478, 209, 2, 1), new Sensor(2, SystemPositionEnum.Bottom)),
+      doorControlRoom = new Room(new RoomDisplaySettings(478, 176, 2, 1), new DoorControl()),
+      cloneBayRoom = new Room(new RoomDisplaySettings(408, 176, 2, 2), new CloneBay(1)),
+      cloackingRoom = new Room(new RoomDisplaySettings(94, 140, 2, 1), new Cloacking(1, 3, false)),
+      droneControlRoom = new Room(new RoomDisplaySettings(129, 176, 2, 2), new DroneControl(1, 13, false)),
+      hackingRoom = new Room(new RoomDisplaySettings(94, 245, 2, 1), new Hacking(1, 0, false)), // Manage advancedEditionEnabled
+      topExternalDoorsRoom = new Room(new RoomDisplaySettings(268, 104, 2, 1)), // Top external doors on center of ship
+      bottomOfTopExternalDoorsRoom = new Room(new RoomDisplaySettings(268, 140, 2, 1)), // Bottom of top external doors
+      topOfBottomExternalDoorsRoom = new Room(new RoomDisplaySettings(268, 246, 2, 1)), // Top of bottom external doors
+      bottomExternalDoorsRoom = new Room(new RoomDisplaySettings(268, 282, 2, 1)), // Bottom external doors on center of ship
+      teleportRoom = new Room(new RoomDisplaySettings(338, 140, 2, 1), new Teleport(1, false)),
+      mindControlRoom = new Room(new RoomDisplaySettings(338, 246, 2, 1), new MindControl(1, 1, false)),
+      batteryRoom = new Room(new RoomDisplaySettings(371, 176, 1, 2), new Battery(1, 6, false));
+
     this.rooms = [
-      new Room(new RoomDisplaySettings(198, 211, 2, 2), new Shield(2, SystemPositionEnum.Left, 3)),
-      new Room(new RoomDisplaySettings(59, 176, 2, 2), new Engine(2, SystemPositionEnum.Bottom), this.crews[1]), // Human Female
-      new Room(new RoomDisplaySettings(302, 176, 1, 2), new Oxygen(1, 6)),
-      new Room(new RoomDisplaySettings(198, 141, 2, 2), new WeaponControl(2, SystemPositionEnum.Top, 2), this.crews[2]), // Lanius
-      new Room(new RoomDisplaySettings(547, 176, 1, 2), new Piloting(1, SystemPositionEnum.Right), this.crews[0]), // Human Male
-      new Room(new RoomDisplaySettings(478, 209, 2, 1), new Sensor(2, SystemPositionEnum.Bottom)),
-      new Room(new RoomDisplaySettings(478, 176, 2, 1), new DoorControl()),
-      new Room(new RoomDisplaySettings(408, 176, 2, 2), new CloneBay(1)),
-      new Room(new RoomDisplaySettings(94, 140, 2, 1), new Cloacking(1, 3, false)),
-      new Room(new RoomDisplaySettings(129, 176, 2, 2), new DroneControl(1, 13, false)),
-      new Room(new RoomDisplaySettings(94, 245, 2, 1), new Hacking(1, 0, false)), // Manage advancedEditionEnabled
-      new Room(new RoomDisplaySettings(268, 104, 2, 1)), // Top external doors on center of ship
-      new Room(new RoomDisplaySettings(268, 140, 2, 1)), // Bottom of top external doors
-      new Room(new RoomDisplaySettings(268, 246, 2, 1)), // Top of bottom external doors
-      new Room(new RoomDisplaySettings(268, 282, 2, 1)), // Bottom external doors on center of ship
-      new Room(new RoomDisplaySettings(338, 140, 2, 1), new Teleport(1, false)),
-      new Room(new RoomDisplaySettings(338, 246, 2, 1), new MindControl(1, 1, false)),
-      new Room(new RoomDisplaySettings(371, 176, 1, 2), new Battery(1, 6, false)),
+      shieldRoom,
+      enginesRoom,
+      oxygenRoom,
+      weaponsControlRoom,
+      pilotingRoom,
+      sensorRoom,
+      doorControlRoom,
+      cloneBayRoom,
+      cloackingRoom,
+      droneControlRoom,
+      hackingRoom,
+      topExternalDoorsRoom,
+      bottomOfTopExternalDoorsRoom,
+      topOfBottomExternalDoorsRoom,
+      bottomExternalDoorsRoom,
+      teleportRoom,
+      mindControlRoom,
+      batteryRoom
     ];
 
     this.weapons = [
@@ -68,5 +88,14 @@ export class KestrelLayoutC extends KestrelShip {
     this.hullSpriteY = 0;
     this.interiorSpriteX = 350;
     this.interiorSpriteY = 97;
+
+    this.shipRepresentation = [
+      [null, null, null, null, null, null, new Slot(0, 0, topExternalDoorsRoom), new Slot(1, 0, topExternalDoorsRoom), null, null, null, null, null, null, null], // y0
+      [null, new Slot(0, 0, cloackingRoom), new Slot(1, 0, cloackingRoom), null, new Slot(0, 0, weaponsControlRoom), new Slot(1, 0, weaponsControlRoom, weaponsControlRoom.affectedCrew), new Slot(0, 0, bottomOfTopExternalDoorsRoom), new Slot(1, 0, bottomOfTopExternalDoorsRoom), new Slot(0, 0, teleportRoom), new Slot(1, 0, teleportRoom), null, null, null, null, null], // y1
+      [new Slot(0, 0, enginesRoom), new Slot(1, 0, enginesRoom), new Slot(0, 0, droneControlRoom), new Slot(1, 0, droneControlRoom), new Slot(0, 1, weaponsControlRoom), new Slot(1, 1, weaponsControlRoom), null, new Slot(0, 0, oxygenRoom), null, new Slot(0, 0, batteryRoom), new Slot(0, 0, cloneBayRoom), new Slot(1, 0, cloneBayRoom), new Slot(0, 0, doorControlRoom), new Slot(1, 0, doorControlRoom), new Slot(0, 0, pilotingRoom, pilotingRoom.affectedCrew)], // y2
+      [new Slot(0, 1, enginesRoom, enginesRoom.affectedCrew), new Slot(1, 1, enginesRoom), new Slot(0, 1, droneControlRoom), new Slot(1, 1, droneControlRoom), new Slot(0, 0, shieldRoom), new Slot(1, 0, shieldRoom), null, new Slot(1, 0, oxygenRoom), null, new Slot(0, 1, batteryRoom), new Slot(0, 1, cloneBayRoom), new Slot(1, 1, cloneBayRoom), new Slot(0, 0, sensorRoom), new Slot(1, 0, sensorRoom), new Slot(0, 1, pilotingRoom)], // y3
+      [null, new Slot(0, 0, hackingRoom), new Slot(1, 0, hackingRoom), null, new Slot(0, 1, shieldRoom), new Slot(1, 1, shieldRoom), new Slot(0, 0, topOfBottomExternalDoorsRoom), new Slot(1, 0, topOfBottomExternalDoorsRoom), new Slot(0, 0, mindControlRoom), new Slot(1, 0, mindControlRoom), null, null, null, null, null], // y4
+      [null, null, null, null, null, null, new Slot(0, 0, bottomExternalDoorsRoom), new Slot(1, 0, bottomExternalDoorsRoom), null, null, null, null, null, null, null], // y5
+    ];
   }
 }
