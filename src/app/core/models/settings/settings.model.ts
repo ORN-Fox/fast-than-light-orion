102b669fe8cc7d4acb0ebb3c38dfc6302944e8c0
Florian Armand <foxdog.orion@gmail.com>
2022-05-25 09:13:59 -0400

    feat: add settings model for store game settings

---
+++ src/app/core/models/settings/settings.model.ts
@@ -0,0 +1,18 @@
+export class Settings {
+
+  // video related
+  fullScreenMode: boolean = false;
+  dynamicBackground: boolean = true;
+  colorblindMode: boolean = false;
+  language: string;
+
+  // gameplay related
+  // TODO
+
+  // audio related
+  volume: number = 1;
+  musicVolume: number = 1;
+
+  constructor()
+  {}
+}
