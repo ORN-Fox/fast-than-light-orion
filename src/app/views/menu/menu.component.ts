aaa7d4fc550f29217a9d9441c7c8e8447c2cb71d
Florian Armand <foxdog.orion@gmail.com>
2022-05-25 08:50:44 -0400

    feat: begin options modal integrations with change language option

--- src/app/views/menu/menu.component.ts
+++ src/app/views/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { GameService } from '../../core/services/game/game.service';
+import { I18nService } from '../../core/services/translations/i18n.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,15 +11,37 @@ import { GameService } from '../../core/services/game/game.service';
 })
 export class MenuComponent implements OnInit {
 
+  languages: string[];
+
   gameInProgress: boolean;
 
   constructor(
     private router: Router,
-    private gameService: GameService
-  ) {}
+    private gameService: GameService,
+    private i18nService: I18nService
+  ) {
+    this.languages = this.i18nService.supportedLanguages;
+  }
 
   ngOnInit(): void {
     this.gameInProgress = this.gameService.shouldExistGameInProgress();
+
+    const modals = document.querySelectorAll("[data-modal]");
+
+    modals.forEach(function (trigger: any) {
+      trigger.addEventListener("click", function (event: any) {
+        event.preventDefault();
+        const modal = document.getElementById(trigger.dataset.modal) as HTMLElement;
+        modal.classList.add("open");
+        const exits = modal.querySelectorAll(".modal-exit");
+        exits.forEach(function (exit) {
+          exit.addEventListener("click", function (event) {
+            event.preventDefault();
+            modal.classList.remove("open");
+          });
+        });
+      });
+    });
   }
 
   continueGame() {
@@ -29,8 +52,11 @@ export class MenuComponent implements OnInit {
     this.router.navigate(['/shed']);
   }
 
-  openSettings() {
-    // TODO
+  // Options related
+
+  changeLanguage(language: string)
+  {
+    this.i18nService.language = language;
   }
 
 }
