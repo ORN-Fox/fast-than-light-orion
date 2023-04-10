e47928a81cd9952692ea4917361ca53f702bd9fc
ORN_Fox <foxdog.orion@gmail.com>
2023-04-10 11:44:51 -0400

    feat: added isOpen property in door model

--- src/app/core/models/door/door.model.ts
+++ src/app/core/models/door/door.model.ts
@@ -5,12 +5,15 @@ export class Door
   rotation: number;
   level: number;
 
+  isOpen: boolean;
+
   constructor(x: number, y: number, rotation: number = 0, level: number = 1) {
     this.x = x;
     this.y = y;
     this.rotation = rotation;
-
     this.level = level;
+
+    this.isOpen = false;
   }
 
   getSrcDoorSprite()
