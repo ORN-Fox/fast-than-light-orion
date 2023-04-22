6925924bd5b1366e39075c3b15f1c6e6ecf99bee
ORN_Fox <foxdog.orion@gmail.com>
2023-04-22 15:28:28 -0400

    chore: clean up components imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,21 @@ import { AppRoutingModule } from './app-routing.module';
 
 // Commons
 import { ModalComponent } from './views/commons/modal/modal.component';
+import { SettingsModalComponent } from './views/commons/settings-modal/settings-modal.component';
 
 // Pages
 import { ShedComponent } from './views/shed/shed.component';
 import { MenuComponent } from './views/menu/menu.component';
 import { GameComponent } from './views/game/game.component';
-import { SettingsModalComponent } from './views/commons/settings-modal/settings-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ModalComponent,
+    SettingsModalComponent,
     ShedComponent,
     MenuComponent,
-    GameComponent,
-    SettingsModalComponent
+    GameComponent
   ],
   imports: [
     BrowserModule,
