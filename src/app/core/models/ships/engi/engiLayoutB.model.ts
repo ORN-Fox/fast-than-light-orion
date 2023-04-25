701e88ad13cf6d2e12353304a2d37e90a20f0208
ORN_Fox <foxdog.orion@gmail.com>
2023-04-25 16:43:58 -0400

    feat: added doors in engi ship model layout B and review rooms for fix errors

--- src/app/core/models/ships/engi/engiLayoutB.model.ts
+++ src/app/core/models/ships/engi/engiLayoutB.model.ts
@@ -36,17 +36,17 @@ export class EngiLayoutB extends EngiShip {
     let shieldRoom = new Room(new RoomDisplaySettings(9, 8, 2, 1), new Shield(2, SystemPositionEnum.Bottom, 11)),
       engineRoom = new Room(new RoomDisplaySettings(5, 3, 2, 2), new Engine(1, SystemPositionEnum.Bottom, 2)),
       oxygenRoom = new Room(new RoomDisplaySettings(6, 5, 1, 2), new Oxygen(1, 6)),
-      weaponControlRoom = new Room(new RoomDisplaySettings(7, 4, 2, 1), new WeaponControl(3, SystemPositionEnum.Top, 8)),
-      droneControlRoom = new Room(new RoomDisplaySettings(5, 7, 2, 2), new DroneControl(3)),
+      weaponControlRoom = new Room(new RoomDisplaySettings(7, 4, 2, 1), new WeaponControl(3, SystemPositionEnum.Top, 11)),
+      droneControlRoom = new Room(new RoomDisplaySettings(5, 7, 2, 2), new DroneControl(3, 4)),
       medbayRoom = new Room(new RoomDisplaySettings(11, 6, 1, 2), new Medbay(1, 6)),
       pilotingRoom = new Room(new RoomDisplaySettings(13, 8, 1, 2), new Piloting(1, SystemPositionEnum.Right), this.crews[0]), // Engi
       doorControlRoom = new Room(new RoomDisplaySettings(7, 7, 2, 1), new DoorControl()),
-      mindControlRoom = new Room(new RoomDisplaySettings(5, 5, 1, 2), new MindControl(1, 2, false)),
+      mindControlRoom = new Room(new RoomDisplaySettings(5, 5, 1, 2), new MindControl(1, 4, false)),
       sensorRoom = new Room(new RoomDisplaySettings(9, 4, 2, 1), new Sensor(1, SystemPositionEnum.Bottom, 2, false)),
       teleprtRoom = new Room(new RoomDisplaySettings(11, 4, 1, 2), new Teleport(1, false)),
-      hackingRoom = new Room(new RoomDisplaySettings(9, 7, 2, 1), new Hacking(1, 2, false)),
-      batteryRoom = new Room(new RoomDisplaySettings(7, 8, 2, 1), new Battery(1, 2, false)),
-      cloackingRoom = new Room(new RoomDisplaySettings(11, 8, 2, 1), new Cloacking(1, 2, false)),
+      hackingRoom = new Room(new RoomDisplaySettings(9, 7, 2, 1), new Hacking(1, 4, false)),
+      batteryRoom = new Room(new RoomDisplaySettings(7, 8, 2, 1), new Battery(1, 7, false)),
+      cloackingRoom = new Room(new RoomDisplaySettings(11, 8, 2, 1), new Cloacking(1, 7, false)),
       topOfWeaponControlRoom = new Room(new RoomDisplaySettings(8, 3, 2, 1));
 
     this.rooms = [
@@ -65,6 +65,33 @@ export class EngiLayoutB extends EngiShip {
       batteryRoom,
       cloackingRoom,
       topOfWeaponControlRoom
+    ];
+
+    this.doors = [
+      new Door(7, 4.5, 90), // Bottom of engine room
+      new Door(6.5, 4), // Right of engine room
+      new Door(4.5, 5), // Left top of mind control room
+      new Door(4.5, 6), // Left bottom of mind control room
+      new Door(5.5, 5), // Left top of oxygen room
+      new Door(5.5, 6), // Left bottom of oxygen room
+      new Door(7, 6.5, 90), // Top of drone control room
+      new Door(6.5, 7), // Left of drone control room
+      new Door(8, 7.5, 90), // Bottom left of door control room
+      new Door(9, 7.5, 90), // Bottom right of door control room
+      new Door(8.5, 7), // Right of door control room
+      new Door(8, 8.5, 90), // Bottom left of battery room
+      new Door(9, 8.5, 90), // Bottom right of battery room
+      new Door(9, 3.5, 90), // Top right of weapon room
+      new Door(8.5, 4), // Right of weapon room
+      new Door(9, 2.5, 90), // Top left of room on top of weapon room
+      new Door(10, 2.5, 90), // Top right of room on top of weapon room
+      new Door(10.5, 4), // Right of sensor room
+      new Door(10, 7.5, 90), // Botton left of battery room
+      new Door(11, 7.5, 90), // Botton right of battery room
+      new Door(10.5, 7), // Right of battery room
+      new Door(12, 5.5, 90), // Bottom of teleport room
+      new Door(12, 7.5, 90), // Bottom of medbay room
+      new Door(12.5, 8) // Right of cloacking room
     ];
 
     this.weapons = [
