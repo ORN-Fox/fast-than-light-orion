0cef58d22555509c4271edbabb1595228cf41fc3
ORN_Fox <foxdog.orion@gmail.com>
2024-05-26 18:58:25 -0400

    chore: clean up debug log in shed resolver

--- src/app/core/resolvers/shed.resolver.ts
+++ src/app/core/resolvers/shed.resolver.ts
@@ -1,17 +1,15 @@
 import { inject } from '@angular/core';
 import { Location } from '@angular/common';
 import { ResolveFn } from '@angular/router';
-
-import { GameService } from '../services/game/game.service';
 import { EMPTY } from 'rxjs';
 
-export const shedResolver: ResolveFn<boolean> = (route, state) => {
+import { GameService } from '../services/game/game.service';
+
+export const shedResolver: ResolveFn<boolean> = () => {
   const gameService = inject(GameService);
   const location = inject(Location);
 
-  console.log('resolver', route, state);
   const gameInProgress = gameService.shouldExistGameInProgress();
-  console.log('gameInProgress', gameInProgress);
   if (gameInProgress) {
     location.back();
     return EMPTY;
